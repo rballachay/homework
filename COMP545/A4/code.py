16258b885fea08d972f07f614cc7e3611121bb0b
rballachay-ecoation <riley.ballachay@ecoation.com>
2024-03-05 14:12:52 -0800

    upload results

--- COMP545/A4/code.py
+++ COMP545/A4/code.py
@@ -9,6 +9,17 @@ from torch.utils.data import DataLoader
 from tqdm.auto import tqdm
 import transformers 
 
+DISTILBERT_CSV = 'results/results_training_distilbert.csv'
+DISTILBERT_TRAINING = 'results/distilbert_training.png'
+
+SOFT_PROMPT_CSV = 'results/results_soft_prompt_engineering.csv'
+SOFT_PROMPT_TRAINING_LOSS = 'results/softprompt_training_loss.png'
+SOFT_PROMPT_TRAINING_ACC = 'results/softprompt_training_accuracy.png'
+
+
+RETRIEVAL_MODEL_RESULTS = 'results/retrieval_model_training.csv'
+MODEL_SAVE_PATH = 'results/bicoder_retrieval_model.pt'
+
 # ######################## PART 1: PROVIDED CODE ########################
 
 def load_datasets(data_directory: str) -> Union[dict, dict]:
@@ -63,6 +74,8 @@ def train_distilbert(model, loader, device):
     criterion = model.get_criterion()
     total_loss = 0.0
 
+    preds = []
+    targets=[]
     for premise, hypothesis, target in tqdm(loader):
         optimizer.zero_grad()
 
@@ -70,6 +83,8 @@ def train_distilbert(model, loader, device):
         target = target.to(device, dtype=torch.float32)
 
         pred = model(inputs)
+        preds.append(pred)
+        targets.append(target)
 
         loss = criterion(pred, target)
         loss.backward()
@@ -77,32 +92,84 @@ def train_distilbert(model, loader, device):
 
         total_loss += loss.item()
 
-    return total_loss / len(loader)
+    return total_loss / len(loader), torch.cat(preds), torch.cat(targets)
 
+def train_soft_prompt(model, sp, loader, device):
+    model.train()
+    criterion = model.get_criterion()
+    total_loss = 0.0
+
+    preds = []
+    targets=[]
+    for premise, hypothesis, target in tqdm(loader):
+        optimizer.zero_grad()
+
+        inputs = model.tokenize(premise, hypothesis).to(device)
+        inputs.input_embedded = sp(model.get_distilbert().embeddings(inputs.input_ids))
+        inputs.attention_mask = pad_attention_mask(inputs.attention_mask, 5)
+
+        target = target.to(device, dtype=torch.float32)
+
+        pred = model(inputs)
+        preds.append(pred)
+        targets.append(target)
+
+        loss = criterion(pred, target)
+        loss.backward()
+        optimizer.step()
+
+        total_loss += loss.item()
+
+    return total_loss / len(loader), torch.cat(preds), torch.cat(targets)
 
 @torch.no_grad()
 def eval_distilbert(model, loader, device):
     model.eval()
 
+    criterion = model.get_criterion()
+
     targets = []
     preds = []
+    losses = []
 
     for premise, hypothesis, target in loader:
-        preds.append(model(model.tokenize(premise, hypothesis).to(device)))
+        pred_ = model(model.tokenize(premise, hypothesis).to(device))
+
+        target = target.to(device, dtype=torch.float32)
+        loss = criterion(pred_, target)
+
+        preds.append(pred_)
 
         targets.append(target)
+        losses.append(loss)
 
-    return torch.cat(preds), torch.cat(targets)
+    # the final round of evaluation
+    for tup in zip(premise[:3],hypothesis[:3],pred_[:3],target[:3]):
+        print(f'Premise: {tup[0]}')
+        print(f'Hypothesis: {tup[1]}')
+        print(f'True Label: {int(tup[3])}')
+        print(f'Predicted Label: {int(tup[2].round())}')
+        print()
+
+    return torch.cat(preds), torch.cat(targets), losses
 
 
 # ######################## PART 1: YOUR WORK STARTS HERE ########################
 class CustomDistilBert(nn.Module):
-    def __init__(self):
+    def __init__(self, name="distilbert-base-uncased"):
         super().__init__()
 
         # TODO: your work below
-        self.distilbert = transformers.DistilBertModel.from_pretrained("distilbert-base-uncased")
-        self.tokenizer = transformers.DistilBertTokenizer.from_pretrained('distilbert-base-uncased')    
+        if name.startswith('distilbert'):
+            self.distilbert = transformers.DistilBertModel.from_pretrained(name)
+            self.tokenizer = transformers.DistilBertTokenizer.from_pretrained(name)    
+        elif name.startswith('roberta'):
+            self.distilbert = transformers.RobertaModel.from_pretrained(name)
+            self.tokenizer = transformers.RobertaTokenizer.from_pretrained(name) 
+        elif name.startswith('bert'):
+            self.distilbert = transformers.BertModel.from_pretrained(name)
+            self.tokenizer = transformers.BertTokenizer.from_pretrained(name) 
+
         self.pred_layer = nn.Linear(self.distilbert.config.hidden_size, 1)
         self.sigmoid = nn.Sigmoid()
         self.criterion = nn.BCELoss()
@@ -174,7 +241,7 @@ class SoftPrompting(nn.Module):
         
     def forward(self, embedded):
         prompts_broadcast = self.prompts.expand(embedded.size(0), self.p, self.e)  # Shape: [B, L, E]
-        return torch.cat([prompts_broadcast,embedded],axis=1)
+        return torch.cat([prompts_broadcast.to('cpu'),embedded.to('cpu')],axis=1)
 
 
 # ######################## PART 3: YOUR WORK HERE ########################
@@ -221,8 +288,10 @@ def get_topk_indices(Q, P, k: int = None):
     top_k = torch.topk(dot_product, k)
     return top_k.indices, top_k.values
 
-def select_by_indices(indices: Tensor, passages: 'list[str]') -> 'list[str]':
-    return [[passages[value] for value in row] for row in indices]
+def select_by_indices(indices: Tensor, passages: 'list[str]', answer_dict=None) -> 'list[str]':
+    if answer_dict is None:
+        return [[passages[value] for value in row] for row in indices]
+    return [[answer_dict[passages[value]] for value in row] for row in indices]
 
 
 def embed_passages(passages: 'list[str]', model, tokenizer, device='cpu', max_length=512):
@@ -257,7 +326,10 @@ def mean_reciprocal_rank(retrieved_indices: 'list[list[int]]', true_indices: 'li
 
 if __name__ == "__main__":
     import pandas as pd
-    from sklearn.metrics import f1_score  # Make sure sklearn is installed
+    from sklearn.metrics import f1_score, accuracy_score  # Make sure sklearn is installed
+    import os 
+    import seaborn as sns
+    import matplotlib.pyplot as plt
 
     random.seed(2022)
     torch.manual_seed(2022)
@@ -265,7 +337,7 @@ if __name__ == "__main__":
     # Parameters (you can change them)
     sample_size = 2500  # Change this if you want to take a subset of data for testing
     batch_size = 64
-    n_epochs = 2
+    n_epochs = 10
     num_words = 50000
 
     # If you use GPUs, use the code below:
@@ -300,28 +372,84 @@ if __name__ == "__main__":
     model = CustomDistilBert().to(device)
     optimizer = model.assign_optimizer(lr=1e-4)
     
-    for epoch in range(n_epochs):
-        loss = train_distilbert(model, train_loader, device=device)
+    if not os.path.exists(DISTILBERT_CSV):
+        results = {'accuracy':[],'stage':[],'epoch':[]}
+        for epoch in range(n_epochs):
+            loss, preds, targets = train_distilbert(model, train_loader, device=device)
+            train_acc_score = accuracy_score(targets.cpu(), preds.round().cpu())
+            results['accuracy'].append(train_acc_score)
+            results['epoch'].append(epoch+1)
+            results['stage'].append('train')
 
-        preds, targets = eval_distilbert(model, valid_loader, device=device)
-        preds = preds.round()
 
-        score = f1_score(targets.cpu(), preds.cpu())
-        print("Epoch:", epoch)
-        print("Training loss:", loss)
-        print("Validation F1 score:", score)
-        print()
+            preds, targets, _ = eval_distilbert(model, valid_loader, device=device)
+            preds = preds.round()
+
+            score = f1_score(targets.cpu(), preds.cpu())
+            print("Epoch:", epoch)
+            print("Training loss:", loss)
+            print("Validation F1 score:", score)
+            print()
+
+            acc_score = accuracy_score(targets.cpu(), preds.cpu())
+            results['accuracy'].append(acc_score)
+            results['epoch'].append(epoch+1)
+            results['stage'].append('eval')
+
+        results = pd.DataFrame(results)
+        results.to_csv(DISTILBERT_CSV,index=False)
+    else:
+        results = pd.read_csv(DISTILBERT_CSV)
     
-    # ###################### PART 2: TEST CODE ######################
-    freeze_params(model.get_distilbert()) # Now, model should have no trainable parameters
+    sns.set_theme()
+    plot = sns.lineplot(data=results,x='epoch',y='accuracy',hue='stage')
+    fig = plot.get_figure()
+    fig.savefig(DISTILBERT_TRAINING,dpi=200)
 
-    sp = SoftPrompting(p=5, e=model.get_distilbert().embeddings.word_embeddings.embedding_dim)
-    batch = model.tokenize(
-        ["This is a premise.", "This is another premise."],
-        ["This is a hypothesis.", "This is another hypothesis."],
-    )
-    batch.input_embedded = sp(model.get_distilbert().embeddings(batch.input_ids))
-    batch.attention_mask = pad_attention_mask(batch.attention_mask, 5)
+    # ###################### PART 2: TEST CODE ######################
+
+    if not os.path.exists(SOFT_PROMPT_CSV):
+        results = {'accuracy':[],'epoch':[],'model':[], 'loss':[]}
+
+        for name in ['roberta-base','distilbert-base-uncased','bert-base-uncased']:
+
+            model = CustomDistilBert(name=name).to(device)
+            optimizer = model.assign_optimizer(lr=1e-4)
+            sp = SoftPrompting(p=5, e=model.get_distilbert().embeddings.word_embeddings.embedding_dim)
+            for epoch in range(n_epochs):
+                loss, preds, targets = train_soft_prompt(model, sp, train_loader, device=device)
+
+                preds, targets, losses = eval_distilbert(model, valid_loader, device=device)
+                preds = preds.round()
+
+                score = f1_score(targets.cpu(), preds.cpu())
+                print("Epoch:", epoch)
+                print("Training loss:", loss)
+                print("Validation F1 score:", score)
+                print()
+
+                acc_score = accuracy_score(targets.cpu(), preds.cpu())
+                results['accuracy'].append(acc_score)
+                results['loss'].append(float(sum(losses)/len(losses)))
+                results['epoch'].append(epoch+1)
+                results['model'].append(name)
+
+        results = pd.DataFrame(results)
+        results.to_csv(SOFT_PROMPT_CSV,index=False)
+    else:
+        results = pd.read_csv(SOFT_PROMPT_CSV)
+
+    plt.clf()
+    sns.set_theme()
+    plot = sns.lineplot(data=results,x='epoch',y='accuracy',hue='model')
+    fig = plot.get_figure()
+    fig.savefig(SOFT_PROMPT_TRAINING_ACC,dpi=200)
+
+    plt.clf()
+    sns.set_theme()
+    plot = sns.lineplot(data=results,x='epoch',y='loss',hue='model')
+    fig = plot.get_figure()
+    fig.savefig(SOFT_PROMPT_TRAINING_LOSS,dpi=200)
 
     # ###################### PART 3: TEST CODE ######################
     # Preliminary
@@ -340,7 +468,6 @@ if __name__ == "__main__":
     name = 'google/electra-small-discriminator'
     q_enc, a_enc, tokenizer = load_models_and_tokenizer(q_name=name, a_name=name, t_name=name)
     
-
     # Tokenize batch and get class output
     q_batch, a_batch = tokenize_qa_batch(tokenizer, q_titles, q_bodies, answers)
 
@@ -372,3 +499,123 @@ if __name__ == "__main__":
     print("Recall@k:", recall_at_k(retrieved_indices, true_indices, k=3))
 
     print("MRR:", mean_reciprocal_rank(retrieved_indices, true_indices))
+
+    class BiEncoder(nn.Module):
+        def __init__(self, q_enc, a_enc):
+            super().__init__()
+            self.q_enc = q_enc
+            self.a_enc = a_enc
+            self.optimizer = torch.optim.Adam(self.parameters(),lr=1e-4)
+
+    model = BiEncoder(q_enc=q_enc, a_enc=a_enc).to(device)
+    optimizer = model.optimizer
+
+    if os.path.exists(MODEL_SAVE_PATH):
+        model.load_state_dict(torch.load(MODEL_SAVE_PATH))
+    else:
+        batch_size = 128
+
+        results = {'recall':[],'epoch':[],'stage':[]}
+        for epoch in range(10):
+                model.train()
+                for i in range(len(qa_data['train'])//batch_size):
+                    q_titles = qa_data['train'].loc[i*batch_size:(i+1)*batch_size, 'QuestionTitle'].tolist()
+                    q_bodies = qa_data['train'].loc[i*batch_size:(i+1)*batch_size, 'QuestionBody'].tolist()
+                    answers = qa_data['train'].loc[i*batch_size:(i+1)*batch_size, 'Answer'].tolist()
+
+                    q_batch, a_batch = tokenize_qa_batch(tokenizer, q_titles, q_bodies, answers)
+
+                    q_out = get_class_output(q_enc, q_batch.to(device))
+                    a_out = get_class_output(a_enc, a_batch.to(device))
+
+                    # Implement in-batch negative sampling
+                    S = inbatch_negative_sampling(q_out, a_out)
+
+                    # Implement contrastive loss
+                    loss = contrastive_loss_criterion(S)
+
+                    optimizer.zero_grad()
+                    loss.backward()
+                    optimizer.step()
+
+                    print(f"Loss: {loss} at epoch {epoch+1}")
+
+                model.eval()
+
+                answer_dict = dict(zip(qa_data['answers']['Answer'],qa_data['answers']['AnswerId']))
+
+                recalls = []
+                for i in range(len(qa_data['valid'])//batch_size):
+                    titles = qa_data['valid'].loc[i*batch_size:(i+1)*batch_size, 'QuestionTitle'].tolist()
+                    bodies = qa_data['valid'].loc[i*batch_size:(i+1)*batch_size, 'QuestionBody'].tolist()
+                    passages = qa_data['valid'].loc[i*batch_size:(i+1)*batch_size, 'Answer'].tolist()
+                    true_indices = qa_data['valid'].loc[i*batch_size:(i+1)*batch_size, 'AnswerId'].tolist()
+
+                    Q = embed_questions(titles, bodies, model=q_enc, tokenizer=tokenizer, max_length=64, device=device)
+                    P = embed_passages(passages, model=a_enc, tokenizer=tokenizer, max_length=64, device=device)
+    
+                    indices, scores = get_topk_indices(Q, P, k=5)
+
+                    selected_indices = select_by_indices(indices, passages, answer_dict)      
+
+                    recall = recall_at_k(selected_indices, true_indices, k=5)          
+
+                    print("Recall@k:", recall)
+                    recalls.append(recall)
+
+                results['epoch'].append(epoch+1)
+                results['recall'].append(sum(recalls)/len(recalls))
+                results['stage'].append('eval')
+
+                recalls = []
+                for i in range(len(qa_data['train'])//batch_size):
+                    titles = qa_data['train'].loc[i*batch_size:(i+1)*batch_size, 'QuestionTitle'].tolist()
+                    bodies = qa_data['train'].loc[i*batch_size:(i+1)*batch_size, 'QuestionBody'].tolist()
+                    passages = qa_data['train'].loc[i*batch_size:(i+1)*batch_size, 'Answer'].tolist()
+                    true_indices = qa_data['train'].loc[i*batch_size:(i+1)*batch_size, 'AnswerId'].tolist()
+
+                    Q = embed_questions(titles, bodies, model=q_enc, tokenizer=tokenizer, max_length=64, device=device)
+                    P = embed_passages(passages, model=a_enc, tokenizer=tokenizer, max_length=64, device=device)
+    
+                    indices, scores = get_topk_indices(Q, P, k=5)
+
+                    selected_indices = select_by_indices(indices, passages, answer_dict)      
+
+                    recall = recall_at_k(selected_indices, true_indices, k=5)          
+
+                    print("Recall@k:", recall)
+                    recalls.append(recall)
+
+                results['epoch'].append(epoch+1)
+                results['recall'].append(sum(recalls)/len(recalls))
+                results['stage'].append('train')
+
+        
+        results = pd.DataFrame(results)
+        results.to_csv(RETRIEVAL_MODEL_RESULTS,index=False)
+
+        # save the model after training and re-load when needed
+        torch.save(model.state_dict(), MODEL_SAVE_PATH)
+
+    # load the model or use just saved model
+    titles = qa_data['valid'].loc[55:58, 'QuestionTitle'].tolist()
+    bodies = qa_data['valid'].loc[55:58, 'QuestionBody'].tolist()
+    passages = qa_data['valid'].loc[55:58, 'Answer'].tolist()
+
+    answers_all = qa_data['answers'].loc[:,'Answer'].astype(str).tolist()
+
+    # we simply don't have enough memory to use all the answers. going to use 
+    # our answers and 1997 other random ones
+    answers_all = random.sample(answers_all, 1997) + passages 
+
+    Q = embed_questions(titles, bodies, model=q_enc, tokenizer=tokenizer, max_length=16, device=device)
+    P = embed_passages(answers_all, model=a_enc, tokenizer=tokenizer, max_length=16, device=device)
+
+    indices, scores = get_topk_indices(Q, P, k=1)
+    predictions = select_by_indices(indices, answers_all)   
+
+    for title, body, passage, prediction in zip(titles,bodies,passages,predictions):
+        print('Question Title: ', title)
+        print('Question Body: ', body)
+        print('True Answer: ', passage)
+        print('Predicted Answer: ', prediction[0])
